4911930a769fc4ad3d9f4b50b1995ac0e2ed0af8
Jean Lauliac <jean@lauliac.com>
2017-08-05 22:49:43 +0200

    some parsing fragments

--- configure.js
+++ configure.js
@@ -4,8 +4,39 @@ const {Manifest} = require('@jeanlauliac/upd-configure');
 const {execSync} = require('child_process');
 
 const manifest = new Manifest();
-
 const BUILD_DIR = '.build_files';
+
+function getFlexCli() {
+  const binPath = execSync('which flex', {encoding: 'utf8'}).split('\n')[0];
+  const version =execSync(`${binPath} --version`, {encoding: 'utf8'})
+    .split('\n')[0];
+  if (!/^flex 2.5/.test(version)) {
+    throw new Error('Flex 2.5 is required');
+  }
+  return manifest.cli_template(binPath, [
+    {literals: ["-o"], variables: ["output_file", "input_files"]},
+  ]);
+}
+
+function getBisonCli() {
+  const binPath = execSync('which bison', {encoding: 'utf8'}).split('\n')[0];
+  const version = execSync(`${binPath} --version`, {encoding: 'utf8'})
+    .split('\n')[0];
+  if (!/^bison \(GNU Bison\) 2\.3$/.test(version)) {
+    throw new Error('GNU Bison 2.3 is required');
+  }
+  return manifest.cli_template(binPath, [
+    {literals: ["-do"], variables: ["output_file", "input_files"]},
+  ]);
+}
+
+const compiled_flex_files = manifest.rule(getFlexCli(), [
+  manifest.source("(src/**/*.lx)"),
+], `${BUILD_DIR}/($1).cpp`);
+
+const compiled_bison_files = manifest.rule(getBisonCli(), [
+  manifest.source("(src/**/*.bs)"),
+], `${BUILD_DIR}/($1).cpp`);
 
 const compile_cpp_cli = manifest.cli_template('clang++', [
   {literals: ["-c", "-o"], variables: ["output_file"]},
@@ -21,23 +52,7 @@ const compile_cpp_cli = manifest.cli_template('clang++', [
 
 const compiled_cpp_files = manifest.rule(compile_cpp_cli, [
   manifest.source("(src/**/*).cpp"),
-], `${BUILD_DIR}/($1).o`);
-
-const flexPath = execSync('which flex', {encoding: 'utf8'}).split('\n')[0];
-const flexVersion =
-  execSync(`${flexPath} --version`, {encoding: 'utf8'})
-    .split('\n')[0];
-if (!/^flex 2.5/.test(flexVersion)) {
-  throw new Error('Flex 2.5 is required');
-}
-
-const compile_flex_cli = manifest.cli_template(flexPath, [
-  {literals: ["-o"], variables: ["output_file", "input_files"]},
-]);
-
-const compiled_flex_files = manifest.rule(compile_flex_cli, [
-  manifest.source("(src/**/*.lx)"),
-], `${BUILD_DIR}/($1).cpp`)
+], `${BUILD_DIR}/($1).o`, [compiled_bison_files]);
 
 const compile_flex_cpp_cli = manifest.cli_template('clang++', [
   {literals: ["-c", "-o"], variables: ["output_file"]},
@@ -57,6 +72,10 @@ const compiled_flex_cpp_files = manifest.rule(compile_flex_cpp_cli, [
   compiled_flex_files,
 ], `${BUILD_DIR}/($1).o`);
 
+const compiled_bison_cpp_files = manifest.rule(compile_flex_cpp_cli, [
+  compiled_bison_files,
+], `${BUILD_DIR}/($1).o`);
+
 manifest.rule(
   manifest.cli_template('clang++', [
     {literals: ["-o"], variables: ["output_file"]},
@@ -66,7 +85,7 @@ manifest.rule(
       variables: ["input_files"]
     },
   ]),
-  [compiled_cpp_files, compiled_flex_cpp_files],
+  [compiled_cpp_files, compiled_flex_cpp_files, compiled_bison_cpp_files],
   "dist/minimale"
 );
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
+#include "../.build_files/src/lib/parsing.bs.hpp"
 #include <iostream>
+#include <memory>
 
-extern "C" int yylex();
+extern "C" int yyparse();
+extern FILE* yyin;
 
 namespace minimale {
 
+typedef std::unique_ptr<FILE, decltype(&fclose)> unique_file;
+
 int run(int argc, char *argv[]) {
-  yylex();
+  unique_file file(fopen(argv[1], "r"), fclose);
+  if (!file) {
+    throw std::runtime_error("cannot open file");
+  }
+  yyin = file.get();
+
+  do {
+    yyparse();
+  } while (!feof(yyin));
+
   return 0;
 }
 
+}
+
+void yyerror(const char *s) {
+  throw std::runtime_error(std::string("parse error: ") + s);
 }
 
 int main(int argc, char *argv[]) {
