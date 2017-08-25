fc971830af6474cd4b77679c2565d2c2c1e35072
Jean Lauliac <jean@lauliac.com>
2017-08-26 00:22:20 +0100

    lang_variants

--- src/lib/lang.h
+++ src/lib/lang.h
@@ -1,14 +1,10 @@
 #pragma once
 
+#include "../../.build_files/src/lib/lang_variants.json.h"
 #include <vector>
 
 namespace minimale {
 namespace lang {
-
-struct unit_stmt;
-struct comp_stmt;
-struct expr;
-struct type_annot;
 
 struct ltr_string { std::string val; };
 struct ltr_int { size_t val; };
@@ -32,15 +28,43 @@ struct comp_field {
 struct comp_func {
   std::string name;
   std::vector<func_arg> args;
+  std::vector<func_stmt> stmts;
 };
 
 struct unit {
   std::vector<unit_stmt> stmts;
 };
 
-struct xml_text { std::string text; }
+struct xml_text { std::string text; };
 struct xml_interp { expr expr; };
 struct xml_tag { std::string name; std::vector<xml_frag> frags; };
+
+struct obj_type_field {
+  std::string name;
+  type_annot type;
+};
+
+struct obj_type_annot {
+  std::vector<obj_type_field> fields;
+};
+
+struct return_stmt {
+  expr expr;
+};
+
+struct assigment {
+  expr assigned;
+  expr value;
+};
+
+struct ref {
+  std::string ident;
+};
+
+struct member_access {
+  expr expr;
+  std::string member;
+};
 
 }
 }
--- tools/gen_variants.js
+++ tools/gen_variants.js
@@ -7,6 +7,7 @@ const path = require('path');
 function main() {
   const depPath = process.argv[2];
   const outPath = process.argv[3];
+  const isCpp = path.extname(outPath) === '.cpp';
   const outDirname = path.dirname(outPath);
   const inPath = process.argv[4];
   const inDirname = path.dirname(inPath);
@@ -15,39 +16,58 @@ function main() {
   deps.end();
   const input = JSON.parse(fs.readFileSync(inPath, 'utf8'));
   const st = fs.createWriteStream(outPath);
-  st.write('#pragma once\n\n');
-  for (const name of input.include) {
-    const incPath = path.join(inDirname, name);
-    st.write(`#include "${path.relative(outDirname, incPath)}"\n`);
+  if (!isCpp) st.write('#pragma once\n\n');
+  if (isCpp) {
+    for (const name of input.include) {
+      const incPath = path.join(inDirname, name);
+      st.write(`#include "${path.relative(outDirname, incPath)}"\n`);
+    }
+    st.write('\n');
   }
-  st.write('\n');
   for (const name of input.namespace) {
     st.write(`namespace ${name} {\n`);
   }
   st.write(`\n`);
+  if (!isCpp) {
+    const typeset = new Set();
+    for (const [name, types] of Object.entries(input.variant_types)) {
+      for (const [i, typename] of types.entries()) {
+        if (typeset.has(typename)) continue;
+        st.write(`struct ${typename};\n`);
+        typeset.add(typename);
+      }
+    }
+    st.write(`\n`);
+  }
   for (const [name, types] of Object.entries(input.variant_types)) {
-    st.write(`struct ${name} {\n`);
-    st.write(`  ${name} (): p_(nullptr, nullptr), t_(-1) {}\n`);
-    st.write(`  ${name} (const ${name}&) = delete;\n`);
-    st.write(`  ${name} (${name}&& x): p_(std::move(x.p_)), t_(x.t_) {}\n`);
-    st.write(`  ${name}& operator=(const ${name}& x) = delete;\n`);
-    st.write(`  ${name}& operator=(${name}&& x) { p_ = std::move(x.p_); t_ = x.t_; return *this; };\n`);
+    if (!isCpp) st.write(`struct ${name} {\n`);
+    const funcPrefix = isCpp ? `${name}::` : '';
+    const indent = isCpp ? '' : '  ';
+    st.write(`${indent}${funcPrefix}${name}()${isCpp ? ': p_(nullptr, nullptr), t_(-1) {}' : ';'}\n`);
+    if (!isCpp) st.write(`${indent}${name}(const ${name}&) = delete;\n`);
+    st.write(`${indent}${funcPrefix}${name}(${name}&& x)${isCpp ? ': p_(std::move(x.p_)), t_(x.t_) {}' : ';'}\n`);
+    if (!isCpp) st.write(`${indent}${name}& ${funcPrefix}operator=(const ${name}& x) = delete;\n`);
+    st.write(`${indent}${name}& ${funcPrefix}operator=(${name}&& x)${isCpp ? ' { p_ = std::move(x.p_); t_ = x.t_; return *this; }' : ';'}\n`);
     st.write(`\n`);
     for (const [i, typename] of types.entries()) {
-      st.write(`  ${name}(${typename}&& value):\n`)
-      st.write(`    p_(reinterpret_cast<void*>(new ${typename}(value)), &delete_${typename}_), t_(${i}) {}\n`);
-      st.write(`  bool is_${typename}() const { return t_ == ${i}; }\n`);
-      st.write(`  const ${typename}& as_${typename}() const { return *reinterpret_cast<${typename}*>(p_.get()); }\n`);
-      st.write(`  ${typename}& as_${typename}() { return *reinterpret_cast<${typename}*>(p_.get()); }\n\n`);
+      st.write(`${indent}${funcPrefix}${name}(${typename}&& value)${isCpp ? ':' : ';'}\n`)
+      if (isCpp) st.write(`    p_(reinterpret_cast<void*>(new ${typename}(value)), &delete_${typename}_), t_(${i}) {}\n`);
+      if (!isCpp) {
+        st.write(`${indent}bool is_${typename}() const { return t_ == ${i}; }\n`);
+        st.write(`${indent}const ${typename}& as_${typename}() const { return *reinterpret_cast<${typename}*>(p_.get()); }\n`);
+        st.write(`${indent}${typename}& as_${typename}() { return *reinterpret_cast<${typename}*>(p_.get()); }\n\n`);
+      }
     }
-    st.write(`private:\n`);
+    if (!isCpp) st.write(`private:\n`);
     for (const [i, typename] of types.entries()) {
-      st.write(`  static void delete_${typename}_(void* p) { delete reinterpret_cast<${typename}*>(p); }\n`);
+      st.write(`${indent}${isCpp ? 'static ' : ''}void ${funcPrefix}delete_${typename}_(void* p)${isCpp ? ` { delete reinterpret_cast<${typename}*>(p); }` : ';'}\n`);
     }
     st.write(`\n`);
-    st.write(`  std::unique_ptr<void, void(*)(void*)> p_;\n`);
-    st.write(`  char t_;\n`);
-    st.write(`};\n\n`);
+    if (!isCpp) {
+      st.write(`  std::unique_ptr<void, void(*)(void*)> p_;\n`);
+      st.write(`  char t_;\n`);
+    }
+    if (!isCpp) st.write(`};\n\n`);
   }
   for (const name of input.namespace) {
     st.write('}\n');
