04149c5b811d7b680a0e3b01b626e42f5143b53d
Jean Lauliac <jean@lauliac.com>
2017-09-02 21:19:37 +0100

    parse xml attrs

--- src/lib/lang.h
+++ src/lib/lang.h
@@ -38,7 +38,17 @@ struct unit {
 
 struct xml_text { std::string val; };
 struct xml_interp { expr expr; };
-struct xml_tag { std::string name; std::vector<xml_frag> frags; };
+
+struct xml_attr {
+  std::string name;
+  expr val;
+};
+
+struct xml_tag {
+  std::string name;
+  std::vector<xml_attr> attrs;
+  std::vector<xml_frag> frags;
+};
 
 struct obj_type_field {
   std::string name;
